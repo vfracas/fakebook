2bb572a876a9b0be8298f3b51352c76886367699
vfracas <vincent.fracas@gmail.com>
2020-09-03 20:17:05 +0200

    login and signup UI, auth with firebase

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { AuthProvider } from './Auth';
+import PrivateRoute from "./PrivateRoute";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+          <PrivateRoute exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/signup" component={SignUp}/>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
---
+++ src/SignUp.js
@@ -0,0 +1,70 @@
+import React, {useCallback} from "react";
+import {withRouter} from "react-router";
+import app from "./base.js";
+
+const SignUp = ({ history}) => {
+    const handleSignUp = useCallback(async event => {
+        event.preventDefault();
+        const {email, password} = event.target.elements;
+        try {
+            await app
+            .auth()
+            .createUserWithEmailAndPassword(email.value, password.value);
+            history.push("/");
+        } catch (error) {
+            alert(error);
+        }
+    }, [history]);
+
+    return(
+        <div className="log_page">
+            <div className="header">
+                <div className="d-flex flex-row justify-content-between align-items-center title_login">
+                    <div className="d-flex flex-column title_sentence">
+                        <h1 className="title"><b>fakebook</b></h1>
+                        <p className="sentence">Avec Fakebook, arborez votre masque et rencontrez de parfaits inconnus.</p>
+                    </div>
+                    <div className="form-group log_form">
+                        <form className="log_inp shadow rounded" onSubmit={handleSignUp}>
+                            <div className="form-group">
+                                <input className="form-control form-control-lg mail" type="email" name="email" placeholder="Adresse e-mail ou numéro de tél."/>
+                            </div>
+                            <div className="form-group">
+                                <input className="form-control form-control-lg form-control-block psw" type="password" name="password" placeholder="Mot de passe"/>
+                            </div>
+                            <button className="btn btn-primary btn-lg btn-block connect_btn" type="submit"><b>S'inscrire</b></button>
+                            <hr className="midline"/>
+                            <button className="signup_btn" type="button"><a className="signup_link" href="/login"><b>Se connecter</b></a></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <p className="languages">
+                    <a href="/login_fr">Français (France)</a>
+                    <a href="/login_us">English (US)</a>
+                </p>
+                <hr/>
+                <p className="services">
+                    <a href="/signup">S'inscrire</a>
+                    <a href="/login">Connexion</a>
+                    <a href="/signup">Instagram</a>
+                    <a href="/login">Messenger</a>
+                    <a href="/login">Fakebook Lite</a>
+                    <a href="/login">Confidentialité</a>
+                    <a href="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal">Pages</a>
+                    <a href="/login">Cookies</a>
+                    <a href="https://www.google.com/maps">Lieux</a>
+                    <a href="/login">Aide</a>
+                    <a href="/login">Emplois</a>
+                    <a href="/login">À propos</a>
+                    <a href="/login">Local</a>
+                    <a href="https://github.com/vfracas">Github</a>
+                </p>
+                <p className="copyrights">Fakebook © 2020</p>
+            </div>
+        </div>
+    );
+};
+
+export default withRouter(SignUp);
