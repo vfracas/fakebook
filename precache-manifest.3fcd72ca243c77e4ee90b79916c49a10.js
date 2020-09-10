8f65744048eb1b99078945b43e3776a57e761a73
vfracas <vincent.fracas@gmail.com>
2020-09-10 14:00:00 +0200

    Updates

---
+++ precache-manifest.3fcd72ca243c77e4ee90b79916c49a10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04978517998b0ed5353f7a4a41480bcc",
+    "url": "/fakebook/index.html"
+  },
+  {
+    "revision": "6539c72d2ea1ac7a0872",
+    "url": "/fakebook/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "7d3079060240b4b44abd",
+    "url": "/fakebook/static/css/main.d9450eb1.chunk.css"
+  },
+  {
+    "revision": "6539c72d2ea1ac7a0872",
+    "url": "/fakebook/static/js/2.51c98f31.chunk.js"
+  },
+  {
+    "revision": "a4e7c04ab80d5657dd83a32aaffe8eec",
+    "url": "/fakebook/static/js/2.51c98f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3079060240b4b44abd",
+    "url": "/fakebook/static/js/main.01e12719.chunk.js"
+  },
+  {
+    "revision": "cdc298b1ecddd2f31987",
+    "url": "/fakebook/static/js/runtime-main.fe3581f8.js"
+  }
+]);
